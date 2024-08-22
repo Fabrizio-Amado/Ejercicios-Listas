7dc2ab00139d826f66422fb8bf8ed102d74b858b
Fabrizio-Amado <133532838+Fabrizio-Amado@users.noreply.github.com>
2024-08-22 16:08:39 -0300

    Add files via upload

---
+++ Tetris/Tetris/Pieza.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class Pieza
+    {
+        public Pieza()
+        {
+
+        }
+
+
+    }
+}
---
+++ Tetris/Tetris/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tetris;
+
+namespace Tetris
+{
+    internal class Program
+    {
+        static List<Pieza> piezas = new List<Pieza>();
+        static void Main(string[] args)
+        {            
+            piezas.Add(new Linea(1, 5));
+            /*Pieza p = new Pieza("linea");
+
+            while (true)
+            {   
+                //checkear teclado
+                p.derecha();
+            }*/
+
+            piezas[0].GetType = 
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
