072911ec94adb2228027ee62657e7cc446a100d5
Fabrizio-Amado <blockzkid@gmail.com>
2024-08-22 15:40:51 -0300

    Tetris

---
+++ Tetris/Tetris/Linea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class Linea : Pieza
+    {
+        List<Bloque> bloques = new List<Bloque>();
+        public Linea(int x, int y) 
+        {            
+            bloques.Add(new Bloque(x+1, y+1));
+            bloques.Add(new Bloque(x+2, y + 1));
+            bloques.Add(new Bloque(x+3, y+1));
+            bloques.Add(new Bloque(x+4, y+1));
+        }
+    }
+}
